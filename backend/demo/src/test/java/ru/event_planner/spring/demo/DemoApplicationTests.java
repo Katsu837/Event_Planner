2b55de15164d5b171085584d1b67b5f96bfbfe2c
Pablo <parusov.pashka@ya.ru>
2023-07-19 12:48:31 +0300

    .

---
+++ backend/demo/src/test/java/ru/event_planner/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.event_planner.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
